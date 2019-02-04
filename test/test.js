f1b16c87bd1ad373d36baa241fb62da90b9e21fd
Ryan Mottley <ligerzero459@gmail.com>
2019-02-04 12:13:53 -0600

    eslintrc: increase max line length to 100 characters
    Semver: minor

--- test/test.js
+++ test/test.js
@@ -20,3 +20,24 @@ test('lintText', (t) => {
     t.equal(result.warningCount, 1, 'should have used single quotes')
   })
 })
+
+test('lint normal line length, 100 characters', (t) => {
+  lintit.lintText(`'use strict'\n'${'a'.repeat(98)}'\n`, (err, result) => {
+      t.plan(3)
+      t.error(err, 'no error while linting')
+      t.type(result, Object)
+      t.equal(result.errorCount, 0)
+  })
+})
+
+test('lint long line length, 101 characters', (t) => {
+  lintit.lintText(`'use strict'\n'${'a'.repeat(99)}'\n`, (err, result) => {
+      t.plan(4)
+      t.error(err, 'no error while linting')
+      t.type(result, Object)
+      t.equal(result.errorCount, 1)
+
+      var errorMessage = result.results[0].messages[0].message
+      t.equal(errorMessage, 'Line 2 exceeds the maximum line length of 100.')
+  })
+})
